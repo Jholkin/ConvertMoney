93631d86efbe770a0fc5b7a5a4261f5213a53722
jholkin <jhil.palacios@unas.edu.pe>
2020-11-05 10:55:17 -0500

    change from get_file_contents to curl

--- Entities/Convert.php
+++ Entities/Convert.php
@@ -19,7 +19,12 @@ class Convert extends Model
 
     public function valueEuro()
     {
-        $res = json_decode(file_get_contents("https://api.cambio.today/v1/quotes/EUR/PEN/json?quantity=2&key=5235|zA4mrWtCr*f33HFpdF7b~cHLv^*3rrsW"));
+        //$res = json_decode(file_get_contents("https://api.cambio.today/v1/quotes/EUR/PEN/json?quantity=2&key=5235|zA4mrWtCr*f33HFpdF7b~cHLv^*3rrsW"));
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://api.cambio.today/v1/quotes/EUR/PEN/json?quantity=2&key=5235|zA4mrWtCr*f33HFpdF7b~cHLv^*3rrsW");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $res = curl_exec($ch);
+        curl_close($ch);
         $value_euro = $res->result->value;
         return $value_euro;
     }
